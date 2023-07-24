c2c3084dabbd2a3b4e2d0cf05b7363c2980ca7cc
filin0wl <filin.owls@gmail.com>
2023-07-24 16:33:06 +0300

    add solution

--- MarathonUIKitEighthTask/ViewController.swift
+++ MarathonUIKitEighthTask/ViewController.swift
@@ -1,19 +1,48 @@
-//
-//  ViewController.swift
-//  MarathonUIKitEighthTask
-//
-//  Created by Anastasiia Perminova on 24.07.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1500)
+        scrollView.backgroundColor = .white
+        return scrollView
+    }()
+
+    private lazy var avatarImage: UIImageView = {
+        let avatarImage = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+        avatarImage.clipsToBounds = true
+        return avatarImage
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = scrollView
+        setupUI()
+
     }
 
+    private func setupUI(){
+        title = "Avatar"
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        for subview in navigationBar.subviews {
+            if NSStringFromClass(subview.classForCoder).contains("UINavigationBarLargeTitleView") {
 
+                subview.addSubview(avatarImage)
+
+                avatarImage.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    avatarImage.trailingAnchor.constraint(equalTo: subview.trailingAnchor,
+                                                       constant: -16),
+                    avatarImage.topAnchor.constraint(equalTo: subview.bottomAnchor, constant: 16),
+                    avatarImage.bottomAnchor.constraint(equalTo: avatarImage.superview?.bottomAnchor ?? navigationBar.bottomAnchor, constant: -10),
+                    avatarImage.heightAnchor.constraint(equalToConstant: 36),
+                    avatarImage.widthAnchor.constraint(equalToConstant: 36)
+                ])
+            }
+        }
+    }
 }
+
+
 
